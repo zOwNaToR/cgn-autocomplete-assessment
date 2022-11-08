7798c845c2ec9cdd0192ab27f88aaf801b7b5ed3
Omar <omar@email.it>
2022-11-08 12:45:59 +0100

    refactored tests with multiple describe

--- src/components/atoms/Autocomplete/Autocomplete.test.tsx
+++ src/components/atoms/Autocomplete/Autocomplete.test.tsx
@@ -54,125 +54,133 @@ describe('Autocomplete', () => {
       expect(onInputChange).toBeCalledWith("Ciao", "change")
    });
 
-   it("does not show suggestion when input length is not satisfied", () => {
-      const { input, getSuggestionsMenu } = setup()
-
-      userEvent.type(input, 'C')
-      expect(getSuggestionsMenu()).not.toBeInTheDocument();
-
-      userEvent.type(input, 'Ci')
-      expect(getSuggestionsMenu()).not.toBeInTheDocument();
-
-      userEvent.clear(input)
-      expect(getSuggestionsMenu()).not.toBeInTheDocument();
-   });
-
-   it("show suggestion when input length is satisfied", () => {
-      const { input, getSuggestionsMenu, getSuggestionsItems } = setup()
-
-      userEvent.type(input, 'Omar')
-      expect(getSuggestionsMenu()).toBeInTheDocument();
-      expect(getSuggestionsItems()).toHaveLength(1);
-
-      userEvent.clear(input)
-      userEvent.type(input, 'Hey ')
-      expect(getSuggestionsMenu()).toBeInTheDocument();
-      expect(getSuggestionsItems()).toHaveLength(1);
-
-      userEvent.clear(input)
-      userEvent.type(input, 'Cia')
-      expect(getSuggestionsMenu()).toBeInTheDocument();
-      expect(getSuggestionsItems()).toHaveLength(2);
-   });
-
-   it("closes suggestions menu after click outside menu", () => {
-      const { input, label, getSuggestionsMenu } = setup()
-
-      userEvent.type(input, 'Omar')
-      expect(getSuggestionsMenu()).toBeInTheDocument();
-
-      userEvent.click(label)
-      expect(getSuggestionsMenu()).not.toBeInTheDocument()
-   });
-
-   it("restore item text inside input after click outside autocomplete when item was selected", () => {
-      const { input, externalDiv, getSuggestionsItems } = setup()
-
-      // Write "Ciao" and click a suggestion
-      userEvent.type(input, 'Ciao')
-      const itemToClick = getSuggestionsItems().find(x => x.textContent === "Ciao come stai?")
-      expect(itemToClick).toBeDefined()
-      userEvent.click(itemToClick!)
-      // Delete a character and click outsite autocomplete
-      userEvent.type(input, '{backspace}')
-      userEvent.click(externalDiv)
-
-      expect(input).toHaveValue("Ciao come stai?")
-   });
-
-   it("open suggestion menu on input focus when input length is not satisfied", () => {
-      const { input, label, getSuggestionsMenu } = setup()
-
-      userEvent.type(input, 'Omar')
-      expect(getSuggestionsMenu()).toBeInTheDocument();
-
-      userEvent.click(label)
-      expect(getSuggestionsMenu()).not.toBeInTheDocument()
-
-      fireEvent.focus(input)
-      expect(getSuggestionsMenu()).toBeInTheDocument();
-   });
-
-   it("does not open suggestion menu on input focus when input length is not satisfied", () => {
-      const { input, label, getSuggestionsMenu } = setup()
-
-      userEvent.type(input, 'Om')
-      expect(getSuggestionsMenu()).not.toBeInTheDocument();
-
-      userEvent.click(label)
-      expect(getSuggestionsMenu()).not.toBeInTheDocument()
-
-      fireEvent.focus(input)
-      expect(getSuggestionsMenu()).not.toBeInTheDocument();
-   });
-
-   it("pass correct object on item click", () => {
-      const onObjectSelected = jest.fn();
-      const { input, getSuggestionsItems } = setup({ onObjectSelected })
-
-      userEvent.type(input, 'Ciao')
-
-      const itemToClick = getSuggestionsItems().find(x => x.textContent === "Ciao come stai?")
-      expect(itemToClick).toBeDefined()
-      userEvent.click(itemToClick!)
-
-      expect(onObjectSelected).toBeCalledTimes(1)
-      expect(onObjectSelected).toBeCalledWith("Ciao come stai?")
-   });
-
-   it("highlight suggestions correctly when pressing up and down arrow keys", () => {
-      const onSuggestionArrowDown = jest.fn();
-      const onSuggestionArrowUp = jest.fn();
-      const { input, getSuggestionsItems, getSuggestionsMenu } = setup({ onSuggestionArrowDown, onSuggestionArrowUp })
-
-      userEvent.type(input, 'Ciao')
-      userEvent.type(input, `{arrowdown}`)
-      userEvent.type(input, `{arrowdown}`)
-      expect(onSuggestionArrowDown).toBeCalledTimes(2)
-
-      userEvent.type(input, `{arrowup}`)
-      expect(onSuggestionArrowUp).toBeCalledTimes(1)
-
-      // userEvent.type(input, `{arrowdown}`) and userEvent.type(input, `{arrowup}`) have strange behaviour, this will not work
-      // expect(getSuggestionsItems()[0]).toHaveClass("active")
-   });
-
-   it("does not filter suggestions on type with custom filterSuggestions function", () => {
+   it("does not filter suggestions when user types inside input if custom filterSuggestions function is passed", () => {
       const filterSuggestions = () => true
       const { input, getSuggestionsItems, getSuggestionsMenu } = setup({ filterSuggestions })
 
       userEvent.type(input, 'Ciao')
 
       expect(getSuggestionsItems()).toHaveLength(defaultSuggestions.length)
+   });
+
+	describe('check minLength before open suggestions menu', () => {
+      it("does not show suggestion when input length is not satisfied", () => {
+         const { input, getSuggestionsMenu } = setup()
+   
+         userEvent.type(input, 'C')
+         expect(getSuggestionsMenu()).not.toBeInTheDocument();
+   
+         userEvent.type(input, 'Ci')
+         expect(getSuggestionsMenu()).not.toBeInTheDocument();
+   
+         userEvent.clear(input)
+         expect(getSuggestionsMenu()).not.toBeInTheDocument();
+      });
+   
+      it("show suggestion when input length is satisfied", () => {
+         const { input, getSuggestionsMenu, getSuggestionsItems } = setup()
+   
+         userEvent.type(input, 'Omar')
+         expect(getSuggestionsMenu()).toBeInTheDocument();
+         expect(getSuggestionsItems()).toHaveLength(1);
+   
+         userEvent.clear(input)
+         userEvent.type(input, 'Hey ')
+         expect(getSuggestionsMenu()).toBeInTheDocument();
+         expect(getSuggestionsItems()).toHaveLength(1);
+   
+         userEvent.clear(input)
+         userEvent.type(input, 'Cia')
+         expect(getSuggestionsMenu()).toBeInTheDocument();
+         expect(getSuggestionsItems()).toHaveLength(2);
+      });
+   });
+
+	describe('click outside of suggestions menu', () => {
+      it("closes suggestions menu after click outside menu", () => {
+         const { input, label, getSuggestionsMenu } = setup()
+   
+         userEvent.type(input, 'Omar')
+         expect(getSuggestionsMenu()).toBeInTheDocument();
+   
+         userEvent.click(label)
+         expect(getSuggestionsMenu()).not.toBeInTheDocument()
+      });
+   
+      it("restores item text inside input after click outside autocomplete when item was selected", () => {
+         const { input, externalDiv, getSuggestionsItems } = setup()
+   
+         // Write "Ciao" and click a suggestion
+         userEvent.type(input, 'Ciao')
+         const itemToClick = getSuggestionsItems().find(x => x.textContent === "Ciao come stai?")
+         expect(itemToClick).toBeDefined()
+         userEvent.click(itemToClick!)
+         // Delete a character and click outsite autocomplete
+         userEvent.type(input, '{backspace}')
+         userEvent.click(externalDiv)
+   
+         expect(input).toHaveValue("Ciao come stai?")
+      });
+   });
+
+	describe('input focus', () => {
+      it("opens suggestion menu on input focus when input length is satisfied", () => {
+         const { input, label, getSuggestionsMenu } = setup()
+   
+         userEvent.type(input, 'Omar')
+         expect(getSuggestionsMenu()).toBeInTheDocument();
+   
+         userEvent.click(label)
+         expect(getSuggestionsMenu()).not.toBeInTheDocument()
+   
+         fireEvent.focus(input)
+         expect(getSuggestionsMenu()).toBeInTheDocument();
+      });
+   
+      it("does not open suggestion menu on input focus when input length is not satisfied", () => {
+         const { input, label, getSuggestionsMenu } = setup()
+   
+         userEvent.type(input, 'Om')
+         expect(getSuggestionsMenu()).not.toBeInTheDocument();
+   
+         userEvent.click(label)
+         expect(getSuggestionsMenu()).not.toBeInTheDocument()
+   
+         fireEvent.focus(input)
+         expect(getSuggestionsMenu()).not.toBeInTheDocument();
+      });
+   });
+
+	describe('item selection and highlighting', () => {
+      it("passes correct object on item click", () => {
+         const onObjectSelected = jest.fn();
+         const { input, getSuggestionsItems } = setup({ onObjectSelected })
+   
+         userEvent.type(input, 'Ciao')
+   
+         const itemToClick = getSuggestionsItems().find(x => x.textContent === "Ciao come stai?")
+         expect(itemToClick).toBeDefined()
+         userEvent.click(itemToClick!)
+   
+         expect(onObjectSelected).toBeCalledTimes(1)
+         expect(onObjectSelected).toBeCalledWith("Ciao come stai?")
+      });
+
+      it("highlights suggestions correctly when pressing up and down arrow keys", () => {
+         const onSuggestionArrowDown = jest.fn();
+         const onSuggestionArrowUp = jest.fn();
+         const { input, getSuggestionsItems } = setup({ onSuggestionArrowDown, onSuggestionArrowUp })
+   
+         userEvent.type(input, 'Ciao')
+         userEvent.type(input, `{arrowdown}`)
+         userEvent.type(input, `{arrowdown}`)
+         expect(onSuggestionArrowDown).toBeCalledTimes(2)
+   
+         userEvent.type(input, `{arrowup}`)
+         expect(onSuggestionArrowUp).toBeCalledTimes(1)
+   
+         // userEvent.type(input, `{arrowdown}`) and userEvent.type(input, `{arrowup}`) have strange behaviour, this will not work
+         // expect(getSuggestionsItems()[0]).toHaveClass("active")
+      });
    });
 })
--- src/components/molecules/PublicapisAutocomplete/PublicapisAutocomplete.test.tsx
+++ src/components/molecules/PublicapisAutocomplete/PublicapisAutocomplete.test.tsx
@@ -56,50 +56,54 @@ describe('PublicapisAutocomplete', () => {
 		expect(label).toBeInTheDocument();
 	});
 
-	it("should not fetch server when input length is less than 3", async () => {
-		const spy = mockFetch(mockedFetchSuccess)
-		const { input } = setup()
+	describe('check input length before open fetch server', () => {
+		it("should not fetch server when input length is less than 3", async () => {
+			const spy = mockFetch(mockedFetchSuccess)
+			const { input } = setup()
 
-		act(() => userEvent.type(input, 'Da'))
+			act(() => userEvent.type(input, 'Da'))
 
-		await waitFor(() => {
-			expect(spy).not.toHaveBeenCalled();
-		})
+			await waitFor(() => {
+				expect(spy).not.toHaveBeenCalled();
+			})
+		});
+
+		it("should fetch server and open menu when input length is greater or equal 3", async () => {
+			const spy = mockFetch(mockedFetchSuccess)
+			const { input, getSuggestionsMenu } = setup()
+
+			act(() => userEvent.type(input, 'Dam'))
+
+			await waitFor(() => {
+				expect(getSuggestionsMenu()).toBeInTheDocument();
+				expect(spy).toHaveBeenCalledTimes(1);
+			})
+		});
 	});
 
-	it("should fetch server and open menu when input length is greater or equal 3", async () => {
-		const spy = mockFetch(mockedFetchSuccess)
-		const { input, getSuggestionsMenu } = setup()
+	describe('suggestions fetch', () => {
+		it("should render suggestion items with same text to fetched suggestions", async () => {
+			mockFetch(mockedFetchSuccess)
+			const { input, getSuggestionsItems } = setup()
 
-		act(() => userEvent.type(input, 'Dam'))
+			act(() => userEvent.type(input, 'Dam'))
 
-		await waitFor(() => {
-			expect(getSuggestionsMenu()).toBeInTheDocument();
-			expect(spy).toHaveBeenCalledTimes(1);
-		})
-	});
+			await waitFor(() => {
+				expect(getSuggestionsItems()).toHaveLength(mockedFetchSuccess.entries!.length)
+				expect(getSuggestionsItems().map(x => x.textContent)).toEqual(mockedFetchSuccess.entries!.map(x => x.Description));
+			})
+		});
 
-	it("suggestions must be equal to fetched suggestions", async () => {
-		const spy = mockFetch(mockedFetchSuccess)
-		const { input, getSuggestionsItems } = setup()
+		it("shows error when input length is satisfied but no suggestion is found", async () => {
+			mockFetch(mockedFetchEmpty)
+			const { input, getSuggestionsMenu, getError } = setup()
 
-		act(() => userEvent.type(input, 'Dam'))
+			act(() => userEvent.paste(input, 'Ciao'))
 
-		await waitFor(() => {
-			expect(getSuggestionsItems()).toHaveLength(mockedFetchSuccess.entries!.length)
-			expect(getSuggestionsItems().map(x => x.textContent)).toEqual(mockedFetchSuccess.entries!.map(x => x.Description));
-		})
-	});
-
-	it("show error when input length is satisfied but no suggestion is found", async () => {
-		mockFetch(mockedFetchEmpty)
-		const { input, getSuggestionsMenu, getError } = setup()
-
-		act(() => userEvent.paste(input, 'Ciao'))
-
-		await waitFor(() => {
-			expect(getSuggestionsMenu()).not.toBeInTheDocument();
-			expect(getError()).toBeInTheDocument();
-		})
+			await waitFor(() => {
+				expect(getSuggestionsMenu()).not.toBeInTheDocument();
+				expect(getError()).toBeInTheDocument();
+			})
+		});
 	});
 });
